0bb098913e713b09dd530fbc3142fdd687b69bc0
Ust Oldfield <uoldfield@users.noreply.github.com>
2018-02-13 14:09:08 +0000

    Add files via upload

---
+++ AzureFunctions/ScheduledTriggerFunction.cs
@@ -0,0 +1,31 @@
+#r "Microsoft.AnalysisServices.Tabular.DLL"
+#r "Microsoft.AnalysisServices.Core.DLL"
+#r "System.Configuration"
+
+using System;
+using System.Configuration;
+using Microsoft.AnalysisServices.Tabular;
+
+public static void Run(TimerInfo myTimer, TraceWriter log)
+{
+    log.Info($"C# Timer trigger function started at: {DateTime.Now}");  
+    try
+            {
+                Microsoft.AnalysisServices.Tabular.Server asSrv = new Microsoft.AnalysisServices.Tabular.Server();
+                var connStr = ConfigurationManager.ConnectionStrings["AASTabular"].ConnectionString;
+                asSrv.Connect(connStr);
+                Database db = asSrv.Databases["azureadventureworks"];
+                Model m = db.Model;
+                //db.Model.RequestRefresh(RefreshType.Full);     // Mark the model for refresh
+                m.RequestRefresh(RefreshType.Full);     // Mark the model for refresh
+                //m.Tables["Date"].RequestRefresh(RefreshType.Full);     // Mark only one table for refresh
+                db.Model.SaveChanges();     //commit  which will execute the refresh
+                asSrv.Disconnect();
+            }
+            catch (Exception e)
+            {
+                log.Info($"C# Timer trigger function exception: {e.ToString()}");
+            }
+    log.Info($"C# Timer trigger function finished at: {DateTime.Now}"); 
+}
+ 
